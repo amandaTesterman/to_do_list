d7e33767a8e4c2e8d695d5c255f742d619e63ea8
amandaTesterman <102314436+amandaTesterman@users.noreply.github.com>
2023-01-09 11:21:07 -0600

    added the tag form, the tag model, and added all the information to the html

--- to_do_container/todo_site/todo_app/models.py
+++ to_do_container/todo_site/todo_app/models.py
@@ -1,4 +1,19 @@
 from django.db import models
 
+
 # Create your models here.
-class Tasks(Models)
+class Tag(models.Model):
+    name  = models.CharField(max_length=30, unique=True)
+
+
+class Task(models.Model):
+    description = models.CharField(max_length=255)
+    #used only for many to many table relationships, when choosing which model to put it on consider how data is being presented, in finished product in this case when you are lookijng at a task you want to see all the tags attached to the task so we are adding this here.
+    tags = models.ManyToManyField(Tag)
+
+class Comment(models.Model):
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    #the moment you add a comment set time 
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
+    #(what do youwant the foreign key attatched To and, what do you want to o when task is deleted)
